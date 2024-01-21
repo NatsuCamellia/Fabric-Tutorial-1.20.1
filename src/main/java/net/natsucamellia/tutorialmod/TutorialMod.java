96fc9d8836f2cc1698488f80bbeaf5e6e86c7959
NatsuCamellia <natsucamellia@gmail.com>
2024-01-21 11:20:28 +0800

    Add item group Ruby Tutorial Group

--- src/main/java/net/natsucamellia/tutorialmod/TutorialMod.java
+++ src/main/java/net/natsucamellia/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.natsucamellia.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.natsucamellia.tutorialmod.item.ModItemGroups;
 import net.natsucamellia.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,11 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 	}
 }
