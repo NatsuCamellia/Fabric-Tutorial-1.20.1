50006abfd75f0c00ef15c52582deebef6b9d2b0a
NatsuCamellia <natsucamellia@gmail.com>
2024-02-01 16:31:09 +0800

    Add tag for Metal Detector

--- src/main/java/net/natsucamellia/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/natsucamellia/tutorialmod/item/custom/MetalDetectorItem.java
@@ -12,6 +12,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.natsucamellia.tutorialmod.util.ModTags;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -60,7 +61,7 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+        return state.isIn(ModTags.Blocks.METAL_DETECTOR_DETECTABLE_BLOCKS);
     }
 
     @Override
